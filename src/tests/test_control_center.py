a8e2f652f0867e4a53bcdc817f328b33eb692e51
Alif Abdullah <alifabdullah7040@gmail.com>
2026-03-07 17:45:27 -0500

    Going back to ink react

--- src/tests/test_control_center.py
+++
@@ -1,7 +0,0 @@
-from src.tempro.control_center import TaskManager
-
-async def test_input_exists():
-  app = TaskManager()
-  async with app.run_test() as pilot:
-    await pilot.press("h")
-    assert app.query_one("#task_cli").value == 'h'
